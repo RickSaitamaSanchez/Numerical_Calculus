5b3b669b3f818a6b671f122537bd82c258ee47f0
Gabriel Lins <38196422+RickSaitamaSanchez@users.noreply.github.com>
2018-11-12 10:28:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import sys
+import pygame, mutagen.mp3
+song_file = 'sfx/ichwill.mp3'
+mp3 = mutagen.mp3.MP3(song_file)
+pygame.mixer.init(frequency=mp3.info.sample_rate)
+pygame.mixer.music.load(song_file)
+pygame.mixer.music.play()
 sys.path.insert(0, 'modules')
 import menu
 
